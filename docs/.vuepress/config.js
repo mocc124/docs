57f7f70c7ed93ee58dfd715bbc36ae14b33ad170
junnian <2184712327@qq.com>
2022-12-18 22:00:05 +0800

    seo优化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,18 @@
 module.exports = {
   base:"/docs/",
   title: 'junnain Blog',
+  description: "这是 junnian 的博客，记录他的生活。",
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', {
+      name: 'author',
+      content: 'junnian'
+    }],
+    ['meta', {
+      name: 'keywords',
+      content: 'VuePress,博客,个人,前端,CSS,JavaSscript,HTML,Vue,TypeScript,ES6'
+    }]
+  ],
   themeConfig: {
     logo: '/assets/img/logo.png',
     lastUpdated: '最后一次更新',
@@ -13,15 +25,15 @@ module.exports = {
           {
             text: "官方文档",
             items: [
-              { text: 'MDN', link: '/' },
-              { text: '现代JavaScript', link: '/' },
+              { text: 'MDN', link: 'https://developer.mozilla.org/zh-CN/' },
+              { text: '现代JavaScript', link: 'https://zh.javascript.info/' },
             ]
           },
           {
             text: "个人博客",
             items: [
-              { text: '小满zs', link: '/' },
-              { text: '飞跃高山与大洋的鱼', link: '/' },
+              { text: '小满zs', link: 'https://blog.csdn.net/qq1195566313' },
+              { text: '飞跃高山与大洋的鱼', link: 'https://docs.shanyuhai.top/' },
             ]
           }
         ]
