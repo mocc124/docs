cdadf327c6f481e4333cff1feb37b260ee2d59f6
junnian <mrnianj@hotmail.com>
2023-01-06 22:21:38 +0800

    feat: 配置文件模块化

---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,36 @@
+module.exports = [
+    {text: '入门前端开发', items: [
+            { text: '计算机网络', link: '' },
+            { text: '浏览器原理', link: '' },
+            { text: 'HTML/CSS/JavaScript', link: 'https://developer.mozilla.org/zh-CN/' },
+            { text: 'ES6', link: '' },
+            { text: 'CSS3', link: '' },
+            { text: 'RegExp', link: '' },
+            { text: 'Git', link: '/Broad/git/' },
+            { text: 'Echarts', link: '/Broad/Echarts/' },
+        ]
+    },
+    {text: '前端进阶内容', items:[
+            { text: '设计模式', link: '' },
+            { text: 'JS module', link: '/Broad/module/' },
+            { text: 'Vue', link: '/Broad/vue/' },
+            { text: 'React', link: '' },
+            { text: 'Axios', link: '' },
+            { text: 'Less/Sass', link: '' },
+            { text: 'Webpack', link: '' },
+            { text: 'uni-app', link: '' },
+            { text: 'TypeScript', link: '/Broad/ts/' },
+            { text: 'Node.js', link: '' },
+        ]
+    },
+    {text: '前端扩展知识', link: '' },
+    {text: '其它', items: [
+            { text: '关于此项目', link: '/aboutThis/' },
+            { text: '挖坑', link: '' },
+            { text: '填坑', link: '' },
+            { text: '杀手还是忍者', link: '/jsReview/' },
+        ]
+    },
+    { text: '推荐资源/工具 🔗', link: '/Amway/' },
+    { text: 'Github', link: 'https://github.com/mocc124'},
+]
---
+++ docs/.vuepress/config/plugins.js
@@ -0,0 +1,10 @@
+module.exports = {
+    '@vuepress/pwa': {
+        serviceWorker: true,
+        updatePopup: {
+            message: "发现了一些新内容.",
+            buttonText: "刷新"
+        }
+    },
+    '@vuepress/back-to-top': true,
+}
